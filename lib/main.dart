186531412c9834aee2ea1e22f2ace359a1510942
Shubham_W <waghmareshubham132@gmail.com>
2021-12-12 13:49:32 +0530

    Day 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 import 'pages/home_page.dart';
-import 'pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,6 +15,7 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.light,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
+        textTheme: GoogleFonts.baumansTextTheme(),
       ),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
