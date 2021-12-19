5e3f3a28798dcff324d1ab660d6bab16123e9cac
Shubham_W <waghmareshubham132@gmail.com>
2021-12-19 11:06:00 +0530

    Day 10 Completed.

---
+++ lib/models/catlog.dart
@@ -0,0 +1,23 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+}
+ 
+final products = [
+  Item(
+    id: "Exm001",
+    name: "Iphone 12 Pro",
+    desc: "Apple Iphone 12th generation.",
+    price: 999,
+    color: "#33505a",
+    // image:,
+
+
+  )
+]
