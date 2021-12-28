e403e76a2eb8255be13002125ab21c810b78138c
Shubham_W <waghmareshubham132@gmail.com>
2021-12-28 19:11:43 +0530

    Day 12 Completed Successfully. (Creation of List View)

--- lib/models/catlog.dart
+++ lib/models/catlog.dart
@@ -1,23 +1,30 @@
-// class Item {
-//   final String id;
-//   final String name;
-//   final String desc;
-//   final num price;
-//   final String color;
-//   final String image;
+class CatalogModel {
+  static final items = [
+    Item(
+      id: 1,
+      name: "iPhone 12 Pro",
+      desc: "Apple Iphone 12th generation.",
+      price: 999,
+      color: "#33505a",
+      image:
+          "https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-12-pro-r1.jpg",
+    ),
+  ];
+}
 
-//   Item({this.id, this.name, this.desc, this.price, this.color, this.image});
-// }
- 
-// final products = [
-//   Item(
-//     id: "Exm001",
-//     name: "Iphone 12 Pro",
-//     desc: "Apple Iphone 12th generation.",
-//     price: 999,
-//     color: "#33505a",
-//     // image:,
+class Item {
+  final int id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
 
-
-//   )
-// ]
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/models/catlog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
+import 'package:flutter_catalog/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final TestList = List.generate(30, (index) => CatalogModel.items[0]);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -13,12 +17,15 @@ class HomePage extends StatelessWidget {
           textScaleFactor: 1.4,
         ),
       ),
-      body: Center(
-        child: Container(
-          child: Text(
-            "Hello, Welcome to our first flutter project",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-          ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: TestList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: TestList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
